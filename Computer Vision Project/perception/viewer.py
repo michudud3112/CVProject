124315ee1116f05b90d6678122f6c5d03fa37ddd
jaytroy <j.borcel@student.rug.nl>
2024-11-19 21:19:07 +0100

    small cahnge

--- Computer Vision Project/perception/viewer.py
+++ Computer Vision Project/perception/viewer.py
@@ -1,10 +1,10 @@
 #Testing. Requires a Kinect 2 to be connected to the computer and for pyfreect2 to be installed
 
-from pyfreenect2 import PyFreeNect2 as Freenect, Freenect2Device as Device
+from pyfreenect2 import PyFreeNect2 as Freenect, Freenect2Device as Device, getDefaultDeviceSerialNumber
 from pyfreenect2 import SyncMultiFrameListener as FrameListener
 
 # Initialize Kinect
-device = Device()
+device = Device(getDefaultDeviceSerialNumber())
 device.open()
 
 # Set up a frame listener
