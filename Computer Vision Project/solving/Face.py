e31c6665a0d9b93d094caa6cd75e1da383d72b97
michudud3112 <dudek.2182796@studenti.uniroma1.it>
2024-11-20 21:12:48 +0100

    Updated method names

--- Computer Vision Project/solving/Face.py
+++ Computer Vision Project/solving/Face.py
@@ -1,7 +1,8 @@
 class face:
     def __init__(self, state) -> None:
         self.state = state
-    def rotateclockwise(self):
+    def rotate_clockwise(self):
+        '''This method enables a face to rotate 90 degrees (one turn) clockwise around itself'''
         #rotate the corners
         temp = self.state[0][0]
         self.state[0][0],self.state[2][0],self.state[2][2],self.state[0][2] = self.state[2][0],self.state[2][2],self.state[0][2],temp
@@ -11,7 +12,8 @@ class face:
         
         #no need to rotate the centers
         
-    def rotatecounterclockwise(self):
+    def rotate_counterclockwise(self):
+        '''This method enables a face to rotate 90 degrees (one turn) counterclockwise around itself'''
         #rotate corners
         temp = self.state[0][0]
         self.state[0][0],self.state[0][2],self.state[2][2], self.state[2][0] = self.state[0][2],self.state[2][2], self.state[2][0],temp
@@ -20,7 +22,8 @@ class face:
         self.state[0][1],self.state[1][2],self.state[2][1],self.state[1][0] = self.state[1][2],self.state[2][1],self.state[1][0],temp
         
     #I suppose I don't necessarily need this method but I immagine coding it like this might be more efficient
-    def rotate180(self):
+    def rotate_180(self):
+        '''This method allows a face to rotate 180 degrees (two turns) around itself'''
         #rotate corners
         temp = self.state[0][0]
         self.state[0][0],self.state[2][2] = self.state[2][2],temp
@@ -33,7 +36,8 @@ class face:
         temp = self.state[1][0]
         self.state[1][0],self.state[1][2] = self.state[1][2],temp
     
-    def showface(self):
+    def show_face(self):
+        '''Neatly displays the state of a face'''
         print(self.state[0][0], self.state[0][1],self.state[0][2])
         print(self.state[1][0], self.state[1][1],self.state[1][2])
         print(self.state[2][0], self.state[2][1],self.state[2][2])
