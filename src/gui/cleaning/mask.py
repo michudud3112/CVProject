a4738ef4c8698c96812cf5221ed983aa84a19e2d
jaytroy <j.borcel@student.rug.nl>
2025-01-06 20:15:40 +0100

    minor changes

--- src/gui/cleaning/mask.py
+++ src/gui/cleaning/mask.py
@@ -2,7 +2,6 @@ import os
 import cv2
 import numpy as np
 from .rectangle import process_image
-from .info import gather_info
 from .sort import process_coordinates
 import re
 
@@ -61,13 +60,35 @@ class ImageProcessor:
             print(f"Failed to save image at {output_path}")
         return True
 
+    def gather_info(self,image_path):
+        img = cv2.imread(image_path)
+        save_dir = os.path.join(os.path.dirname(__file__), "samples")
+        res2 = cv2.imread(os.path.join(save_dir, "res2.jpg"))
+
+        gray = cv2.cvtColor(res2, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        def get_rectangle_info(contour):
+            x, y, w, h = cv2.boundingRect(contour)
+            return (y, x, x, y, w, h)
+
+        sorted_contours = sorted(contours, key=get_rectangle_info)
+
+        with open(os.path.join(save_dir, "wall.txt"), 'w') as file:
+            for contour in sorted_contours[:9]:
+                x, y, w, h = cv2.boundingRect(contour)
+
+                file.write(f"Rectangle: x={x}, y={y}, width={w}, height={h}\n")
+
     def process_additional_steps(self, intermediate_path, final_path, wall_text, sorted_wall_text):
         image = cv2.imread(intermediate_path)
         if image is None:
             raise FileNotFoundError(f"Image at path {intermediate_path} could not be loaded.")
 
         process_image(intermediate_path, final_path)
-        gather_info(final_path)
+        self.gather_info(final_path)
         process_coordinates(wall_text, final_path, os.path.join(self.save_dir, "res3.jpg"), sorted_wall_text)
         
         try:
@@ -102,3 +123,26 @@ class ImageProcessor:
                                                os.path.join(self.save_dir, "wall.txt"),
                                                os.path.join(self.save_dir, "wall_sorted.txt"))
         return result
+
+
+    def gather_info(self,image_path):
+        img = cv2.imread(image_path)
+        save_dir = os.path.join(os.path.dirname(__file__), "samples")
+        res2 = cv2.imread(os.path.join(save_dir, "res2.jpg"))
+
+        gray = cv2.cvtColor(res2, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        def get_rectangle_info(contour):
+            x, y, w, h = cv2.boundingRect(contour)
+            return (y, x, x, y, w, h)
+
+        sorted_contours = sorted(contours, key=get_rectangle_info)
+
+        with open(os.path.join(save_dir, "wall.txt"), 'w') as file:
+            for contour in sorted_contours[:9]:
+                x, y, w, h = cv2.boundingRect(contour)
+
+                file.write(f"Rectangle: x={x}, y={y}, width={w}, height={h}\n")
